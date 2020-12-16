18a2989a44dcb9d7efeac6a786584b6b03988ed2
davidemanzilli <61005304+davidemanzilli@users.noreply.github.com>
2020-12-17 00:13:09 +0100

    Add files via upload

---
+++ 1-100(2).py
@@ -0,0 +1,5 @@
+x = 0
+
+for x in range (0,101):
+    print (x)
+    x = x+1
---
+++ esercizio-stringhe.py
@@ -0,0 +1,16 @@
+# si modifichi il codice  affinchè
+# data una stringa stampi a video un carattere alla volta.
+
+stringa = str(input("inserisci una frase"))
+
+print(stringa)
+
+i = 0
+while i < len(stringa):
+    print(stringa[i])
+    i = i + 1
+
+
+# stampa a monitor la lunghezza della stringa STRINGA
+
+print("ci sono",i,"caratteri")
