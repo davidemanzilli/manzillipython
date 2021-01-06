aef938088bea3f9ce4433f20f767c5f19644954a
davidemanzilli <61005304+davidemanzilli@users.noreply.github.com>
2021-01-06 12:41:05 +0100

    Add files via upload

--- leggidati2.py
+++ leggidati2.py
@@ -16,7 +16,7 @@ f = open("dati.txt", 'r')
 
 coordX = []
 coordY = []
-
+coordY2 = []
 
 
 # da notare che posso fare un ciclo all'interno di un file
@@ -31,31 +31,40 @@ for riga in f:
     print(valori)
     coordX.append(int(valori[0])) # aggiungo la coordinata X
     coordY.append(int(valori[1])) # aggiungo la coordinata Y
+    coordY2.append(int(valori[1])) # aggiungo la coordinata Y2
 
 f.close()  # chiudiamo il file
 
 print ("X: ",coordX)
 print ("Y: ",coordY)
-
+print ("Y2: ",coordY2)
 
 # ordiniamo le liste
 coordX.sort()
 coordY.sort()
-
+coordY2.sort()
 
 print("liste ordinate:") 
 print ("X: ",coordX)
 print ("Y: ",coordY)
-
+print ("Y2: ",coordY2)
 
 # stampo il tipo di dati delle coordinate
 print(type(coordX))
 print(type(coordY))
-
+print(type(coordY2))
 
 # ora sono pronte per essere usate anche nei grafici
 
+plt.subplot(2,1,1)
+plt.bar(coordX,coordY)
+plt.title("GRAFICO A BARRE", color='#003399')
+plt.ylabel('Y', color='#003399')
+plt.xlabel('X', color='#003399')
+plt.xticks([10*k for k in range(0,11)])
+plt.yticks([10*k for k in range(0,11)])
 
+plt.subplot(2,1,1)
 plt.scatter(coordX,coordY, color='red', alpha=0.5, marker='.')
 plt.title("GRAFICO A DISPERSIONE", color='#003399')
 plt.ylabel('Y', color='#003399')
